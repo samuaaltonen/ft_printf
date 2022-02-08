7dc56fb5f2ffb7076fa24acedc277c8fd00ef54f
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-08 11:44:52 +0200

    Added more tests cases for different flags.

--- eval_tests/printf_tests.sh
+++ eval_tests/printf_tests.sh
@@ -30,7 +30,7 @@ if [ $1 -eq 1 ]; then
 TESTMAIN="tests_main_01"
 gcc -Wall -Wextra -Werror -o tests -I ../ -I ../libft/includes -L ../ -L ../libft -lftprintf -lft -x c $TESTMAIN.test
 
-printf "\n\033[0;35m======================= 01 STRINGS =======================\033[0m\n"
+printf "\n\033[0;35m======================= 01 STRINGS, WIDTH AND ALIGNMENT =======================\033[0m\n"
 printf "Output of libc printf will be cyan and ft_printf purple.\n\n"
 
 ./tests
@@ -42,7 +42,7 @@ if [ $1 -eq 2 ]; then
 TESTMAIN="tests_main_02"
 gcc -Wall -Wextra -Werror -o tests -I ../ -I ../libft/includes -L ../ -L ../libft -lftprintf -lft -x c $TESTMAIN.test
 
-printf "\n\033[0;35m======================= 02 SIMPLE DATA TYPES =======================\033[0m\n"
+printf "\n\033[0;35m======================= 02 SIMPLE DATA TYPES AND FLAGS =======================\033[0m\n"
 printf "Output of libc printf will be cyan and ft_printf purple.\n\n"
 
 ./tests
@@ -88,21 +88,33 @@ fi
 if [ $1 -eq 6 ]; then
 
 TESTMAIN="tests_main_06"
+gcc -Wall -Wextra -Werror -o tests -I ../ -I ../libft/includes -L ../ -L ../libft -lftprintf -lft -x c $TESTMAIN.test
+
+printf "\n\033[0;35m======================= 06 OTHER FLAGS =======================\033[0m\n"
+printf "Output of libc printf will be cyan and ft_printf purple.\n\n"
+
+./tests
+
+fi
+
+if [ $1 -eq 7 ]; then
+
+TESTMAIN="tests_main_07"
 gcc -Wall -Wextra -Werror -o tests_memory -I ../ -I ../libft/includes -L ../ -L ../libft -lftprintf -lft -x c $TESTMAIN.test
 
-printf "\n\033[0;35m======================== 06 MEMORY USAGE ========================\033[0m\n"
+printf "\n\033[0;35m======================== 07 MEMORY USAGE ========================\033[0m\n"
 printf "Calling ft_printf in infinite loop for 20 seconds. Monitor memory \nusage with \033[0;36mleaks tests_memory\033[0m and \033[0;36mtop | grep tests_memory\033[0m\n\n"
 
 ./tests_memory
 
 fi
 
-if [ $1 -eq 7 ]; then
+if [ $1 -eq 8 ]; then
 
-TESTMAIN="tests_main_06"
+TESTMAIN="tests_main_08"
 gcc -Wall -Wextra -Werror -o tests -I ../ -I ../libft/includes -L ../ -L ../libft -lftprintf -lft -x c $TESTMAIN.test
 
-printf "\n\033[0;35m======================== 07 RANDOM TESTS ========================\033[0m\n"
+printf "\n\033[0;35m======================== 08 FUN TESTS ========================\033[0m\n"
 
 ./tests
 
