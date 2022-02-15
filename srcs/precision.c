b73acc1e2266e37c65174fc0d87f4576fbd5f0cf
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-15 17:21:19 +0200

    Now sets zeropadding to false when precision is set.

--- srcs/precision.c
+++ srcs/precision.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 15:08:09 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/15 15:50:15 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/15 17:09:28 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	handle_precision(t_conf **conf, char **cursor)
 
 	if (**cursor == '.')
 	{
+		(*conf)->flag_zeropadded = 0;
 		(*conf)->precision = ft_atoi(*cursor + 1);
 		digits = ft_count_digits((*conf)->precision);
 		if (!ft_isdigit(*(*cursor + 1)))
