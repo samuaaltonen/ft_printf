2631b5b4bfcf3e9420fbeeb855e72ecdd82b82fc
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-15 17:24:06 +0200

    Added precision, width, zeropadding checks for unsigned integer conversion.

--- srcs/conversion_unsigned.c
+++ srcs/conversion_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:03:23 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/15 14:14:29 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/15 17:22:47 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,27 @@ static void	unsigned_output(unsigned long long number, int *n)
 	(*n)++;
 }
 
+static int	unsigned_width(t_conf **conf, long long number)
+{
+	int	width;
+
+	width = ft_count_digits(number);
+	if ((*conf)->precision > width)
+		width = (*conf)->precision;
+	if ((*conf)->flag_addspace)
+		width++;
+	return (width);
+}
+
+static void	unsigned_output_zeroes(t_conf **conf, long long number)
+{
+	int	len;
+
+	len = ft_count_digits(number);
+	if ((*conf)->precision > len)
+		ft_putchar_n_repeat('0', &((*conf)->n), (*conf)->precision - len);
+}
+
 /*
  * Handles unsigned decimal (u) conversion.
 */
@@ -29,17 +50,15 @@ void	conversion_unsigned(t_conf **conf)
 	unsigned long long	number;
 
 	number = get_va_arg_unsigned(conf);
-	len = ft_count_digits_ull(number);
-	if ((*conf)->width - len > 0
+	len = unsigned_width(conf, number);
+	if ((*conf)->width > len
 		&& !(*conf)->flag_leftadjusted
 		&& !(*conf)->flag_zeropadded)
 		ft_putchar_n_repeat(' ', &((*conf)->n), (*conf)->width - len);
-	if ((*conf)->width - len > 0
-		&& !(*conf)->flag_leftadjusted
-		&& (*conf)->flag_zeropadded)
-		ft_putchar_n_repeat('0', &((*conf)->n), (*conf)->width - len);
+	unsigned_output_zeroes(conf, number);
 	unsigned_output(number, &((*conf)->n));
-	if ((*conf)->width - len > 0 && (*conf)->flag_leftadjusted)
+	if ((*conf)->width > len
+		&& (*conf)->flag_leftadjusted
+		&& !(*conf)->flag_zeropadded)
 		ft_putchar_n_repeat(' ', &((*conf)->n), (*conf)->width - len);
-	(*conf)->n += len;
 }
