17161f669ae39ae168d3a40f74e4502e45886824
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-15 17:20:15 +0200

    Now handles precision, widths and flags correctly for integers.

--- srcs/conversion_int.c
+++ srcs/conversion_int.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 11:55:09 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/15 14:02:43 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/15 17:19:15 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,52 @@ static void	int_output(long long number, int *n)
 	(*n)++;
 }
 
+static void	int_output_sign(t_conf **conf, long long number)
+{
+	if (number < 0 || (*conf)->flag_addsign)
+	{
+		if (number < 0)
+			ft_putchar_n('-', &((*conf)->n));
+		else
+			ft_putchar_n('+', &((*conf)->n));
+	}
+}
+
+static int	int_width(t_conf **conf, long long number)
+{
+	int	width;
+
+	width = ft_count_digits(number);
+	if ((*conf)->precision > width)
+		width = (*conf)->precision;
+	if ((*conf)->flag_addsign || number < 0)
+		width++;
+	if ((*conf)->flag_addspace)
+		width++;
+	return (width);
+}
+
+static void	int_output_zeroes(t_conf **conf, long long number)
+{
+	int	len;
+
+	len = ft_count_digits(number);
+	if ((*conf)->precision > len)
+		ft_putchar_n_repeat('0', &((*conf)->n), (*conf)->precision - len);
+	if ((*conf)->flag_zeropadded && (*conf)->width > len)
+	{
+		if (number < 0 || (*conf)->flag_addsign)
+			ft_putchar_n_repeat('0', &((*conf)->n), (*conf)->width - len - 1);
+		else
+			ft_putchar_n_repeat('0', &((*conf)->n), (*conf)->width - len);
+	}
+}
+
 /*
  * Handles signed decimal (d, i) conversion.
+ *
+ * Note: Adds zeropadding when precision is higher than digit count, even
+ *       though when zeropadding flag is not set.
 */
 void	conversion_int(t_conf **conf)
 {
@@ -31,25 +75,16 @@ void	conversion_int(t_conf **conf)
 	long long	number;
 
 	number = get_va_arg(conf);
-	len = ft_count_digits(number);
-	if (number < 0 || (*conf)->flag_addsign)
-	{
-		if (number < 0)
-			ft_putchar_n('-', &((*conf)->n));
-		else
-			ft_putchar_n('+', &((*conf)->n));
-		len++;
-	}
-	if ((*conf)->width - len > 0
+	len = int_width(conf, number);
+	if ((*conf)->width > len
 		&& !(*conf)->flag_leftadjusted
 		&& !(*conf)->flag_zeropadded)
 		ft_putchar_n_repeat(' ', &((*conf)->n), (*conf)->width - len);
-	if ((*conf)->width - len > 0
-		&& !(*conf)->flag_leftadjusted
-		&& (*conf)->flag_zeropadded)
-		ft_putchar_n_repeat('0', &((*conf)->n), (*conf)->width - len);
+	int_output_sign(conf, number);
+	int_output_zeroes(conf, number);
 	int_output(number, &((*conf)->n));
-	if ((*conf)->width - len > 0 && (*conf)->flag_leftadjusted)
+	if ((*conf)->width > len
+		&& (*conf)->flag_leftadjusted
+		&& !(*conf)->flag_zeropadded)
 		ft_putchar_n_repeat(' ', &((*conf)->n), (*conf)->width - len);
-	(*conf)->n += len;
 }
