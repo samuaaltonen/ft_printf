85cfcbf6444f7bf16c76c1b94327e34ed11c9997
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-17 17:45:02 +0200

    Added support for asterix for width and precision.

--- srcs/precision.c
+++ srcs/precision.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 15:08:09 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/15 17:09:28 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/17 17:30:48 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ void	handle_precision(t_conf **conf, char **cursor)
 
 	if (**cursor == '.')
 	{
+		if (*(*cursor + 1) == '*')
+		{
+			(*conf)->precision = va_arg((*conf)->ap, int);
+			(*cursor) += 2;
+			return ;
+		}
 		(*conf)->flag_zeropadded = 0;
 		(*conf)->precision = ft_atoi(*cursor + 1);
 		digits = ft_count_digits((*conf)->precision);
--- srcs/width.c
+++ srcs/width.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 15:08:11 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/10 15:08:33 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/17 17:43:48 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,21 @@
 */
 void	handle_width(t_conf **conf, char **cursor)
 {
+	if (**cursor == '*')
+	{
+		(*conf)->width = va_arg((*conf)->ap, int);
+		if ((*conf)->width < 0)
+		{
+			(*conf)->width *= -1;
+			(*conf)->flag_leftadjusted = 1;
+		}
+		(*cursor)++;
+	}
 	if (ft_isdigit(**cursor))
 	{
 		(*conf)->width = ft_atoi(*cursor);
 		*cursor += ft_count_digits((*conf)->width);
 	}
+	if (**cursor == '*')
+		(*cursor)++;
 }
