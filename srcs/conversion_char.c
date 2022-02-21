1da8f10c89e66c3fad60e91d414bdb016dc723d0
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-21 14:38:11 +0200

    Added zero-padding support for char.

--- srcs/conversion_char.c
+++ srcs/conversion_char.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 13:46:17 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/17 14:17:10 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/21 14:00:07 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	conversion_char(t_conf **conf)
 {
+	char	left_filler;
+
+	left_filler = ' ';
+	if ((*conf)->flag_zeropadded || (*conf)->flag_zeropadded_override)
+		left_filler = '0';
 	if ((*conf)->width - 1 > 0 && !(*conf)->flag_leftadjusted)
-		ft_putchar_n_repeat(' ', &((*conf)->n), (*conf)->width - 1);
+		ft_putchar_n_repeat(left_filler, &((*conf)->n), (*conf)->width - 1);
 	ft_putchar_n(va_arg((*conf)->ap, int), &((*conf)->n));
 	if ((*conf)->width - 1 > 0 && (*conf)->flag_leftadjusted)
 		ft_putchar_n_repeat(' ', &((*conf)->n), (*conf)->width - 1);
