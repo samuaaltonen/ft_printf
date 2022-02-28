6b88c81522650f1d8bf31fd621d92e010cae9050
Samu Aaltonen <saaltone@student.hive.fi>
2022-02-28 14:55:47 +0200

    Added bankers rounding to ft_fa_round (rounds to closes even number when digit is 5).

--- libft/srcs/ft_fa_round.c
+++ libft/srcs/ft_fa_round.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fa_round.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: saaltone <saaltone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 14:03:06 by saaltone          #+#    #+#             */
-/*   Updated: 2022/02/16 18:20:53 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/02/28 14:52:41 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,29 @@ static int	prepend_one(char **number)
 	return (1);
 }
 
+static int	previous_digit_is_even(char **number, int index)
+{
+	if (index < 1)
+		return (0);
+	if ((*number)[index - 1] == '.')
+		return (previous_digit_is_even(number, index - 1));
+	if (((*number)[index - 1] + '0') % 2 == 0)
+		return (1);
+	return (0);
+}
+
+static int	round_down(char **number, int index)
+{
+	if (index < 0)
+		return (0);
+	(*number)[index] = '0';
+	return (1);
+}
+
 /*
  * Rounds float to given precision. Float is inputted as ascii string. Returns
  * 1 if rounding has modified the string.
+ * Uses bankers rounding (rounds to closes even number)
 */
 int	ft_fa_round(char **number, int rounding_start, int leftover)
 {
@@ -37,10 +57,10 @@ int	ft_fa_round(char **number, int rounding_start, int leftover)
 	if (!leftover)
 	{
 		if ((*number)[rounding_start] < '5')
-		{
-			(*number)[rounding_start] = '0';
-			return (1);
-		}
+			return (round_down(number, rounding_start));
+		if ((*number)[rounding_start] == '5'
+			&& previous_digit_is_even(number, rounding_start))
+			return (round_down(number, rounding_start));
 		(*number)[rounding_start] = '0';
 		return (ft_fa_round(number, rounding_start - 1, 1));
 	}
