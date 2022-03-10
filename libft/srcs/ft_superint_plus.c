c25c5cad863a573272278338a1aedea68fea09b8
Samu Aaltonen <saaltone@student.hive.fi>
2022-03-10 22:50:00 +0000

    Added ft_superint_plus_ull to libft (adds unsigned long long to superint).

--- libft/srcs/ft_superint_plus.c
+++ libft/srcs/ft_superint_plus.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 13:45:20 by saaltone          #+#    #+#             */
-/*   Updated: 2022/03/10 21:37:13 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/03/10 22:34:40 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,39 @@ int	ft_superint_plus_int(t_superint **addto, int plus)
 	t_ull	temp;
 	int		i;
 
+	i = 0;
+	while (plus)
+	{
+		if ((*addto)->allocated < i + 1 && !ft_superint_expand(addto))
+			return (0);
+		temp = (t_ull)(*addto)->numbers[i] + plus;
+		plus = 0;
+		if (temp >= 1000000000)
+			plus = temp / 1000000000;
+		if (temp >= 1000000000)
+			(*addto)->numbers[i] = temp % 1000000000;
+		else
+			(*addto)->numbers[i] = temp;
+		if (i > (*addto)->count - 1)
+			(*addto)->count++;
+		i++;
+	}
+	return (1);
+}
+
+/*
+ * Adds unsigned long long to superint.
+*/
+int	ft_superint_plus_ull(t_superint **addto, t_ull plus)
+{
+	t_ull	temp;
+	int		i;
+
+	if (plus <= 2147483647)
+		return (ft_superint_plus_int(addto, plus));
+	if (!ft_superint_plus_int(addto, 2147483647))
+		return (0);
+	plus -= 2147483647;
 	i = 0;
 	while (plus)
 	{
